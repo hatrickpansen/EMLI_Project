e139229c784f6ceb18dd25584a27d0eadfd18fac
TTengs <theistengs@gmail.com>
2023-05-04 14:59:14 +0200

    Remote done

--- remote/remote.ino
+++ remote/remote.ino
@@ -1,7 +1,13 @@
-#include <ESP8266WiFi.h>
-#include "HTTP_Method.h"
+// Embedded Linux (EMLI)
+// University of Southern Denmark
+// ESP8266 Wifi client - Webserver - User interface 
+// Kjeld Jensen <kjen@sdu.dk> <kj@kjen.dk>
+// 2023-04-18, KJ, First version
+// inspired by https://docs.arduino.cc/tutorials/uno-wifi-rev2/uno-wifi-r2-hosting-a-webserver
 
-// LEDS
+#include <PubSubClient.h>
+
+// LED
 #define PIN_LED_RED     14
 #define PIN_LED_YELLOW  13
 #define PIN_LED_GREEN   12
@@ -13,21 +19,62 @@ volatile int button_a_count;
 volatile unsigned long count_prev_time;
 
 // Wifi
-const char* WIFI_SSID = "EMLI";
-const char* WIFI_PASSWORD = "EMLIpassword";
+#include <ESP8266WiFi.h>
+const char* WIFI_SSID = "EMLI_TEAM_08";
+const char* WIFI_PASSWORD = "linuxembedded";
 
 // Static IP address
-IPAddress IPaddress(192, 168, 0, 222);
-IPAddress gateway(192, 168, 0, 1);
+IPAddress IPaddress(192, 168, 10, 222);
+IPAddress gateway(192, 168, 10, 1);
 IPAddress subnet(255, 255, 255, 0);
 //IPAddress primaryDNS(1, 1, 1, 1); 
 //IPAddress secondaryDNS(8, 8, 8, 8); 
 
+//#define MQTT_HOST IPAddress(172, 20, 10, 11)
+#define MQTT_HOST IPAddress(10, 0, 0, 10)
+#define MQTT_PORT 1883
+const char* mqtt_username = "your-mqtt-username";
+const char* mqtt_password = "your-mqtt-password";
+const char* mqtt_topic = "remote/count";
+
 // Webserver
 #define CLIENT_TIMEOUT 2000
-WiFiServer server(80);
 
-void setupLEDS() {
+WiFiServer server(80);
+WiFiClient client = server.available();
+PubSubClient mqttClient(client);
+unsigned long clientConnectTime = 0;
+String currentLine = "";
+char response_s[10];
+char s[25];
+
+ICACHE_RAM_ATTR void button_a_isr()
+{
+  if (millis() - count_prev_time > DEBOUNCE_TIME)
+  {
+    count_prev_time = millis();
+    button_a_count++;
+  }
+}
+
+void buttonCount()
+{
+    char count_str[4];
+    itoa(button_a_count, count_str, 10);
+    
+    if(mqttClient.publish("remote/count", count_str)){
+      Serial.println("Publish to MQTT");
+      button_a_count = 0;
+    }
+    delay(2000);
+}
+
+void setup()
+{
+  // serial
+  Serial.begin(115200);
+  delay(10);
+
   // LEDs
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
@@ -37,12 +84,18 @@ void setupLEDS() {
   digitalWrite(PIN_LED_YELLOW, LOW);
   pinMode(PIN_LED_GREEN, OUTPUT);
   digitalWrite(PIN_LED_GREEN, LOW);
-}
 
-void setupWifi() {
-  // configure as WiFi client
+  // button
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIN_BUTTON), button_a_isr, RISING);
+
+  // set the ESP8266 to be a WiFi-client
   WiFi.mode(WIFI_STA); 
   
+  // configure static IP address
+  WiFi.config(IPaddress, gateway, subnet);
+  //WiFi.config(IPaddress, gateway, subnet, primaryDNS, secondaryDNS);
+
   // connect to Wifi access point
   Serial.println();
   Serial.println();
@@ -67,24 +120,38 @@ void setupWifi() {
   Serial.println("");
   server.begin();
 
-  // configure static IP address
-  WiFi.config(IPaddress, gateway, subnet);
-  //WiFi.config(IPaddress, gateway, subnet, primaryDNS, secondaryDNS);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  while (!mqttClient.connected()) {
+    Serial.println("Connecting to MQTT broker...");
+    if (mqttClient.connect("ESP8266Client")) {
+      Serial.println("Connected to MQTT broker");
+    } else {
+      Serial.print("Failed to connect to MQTT broker, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" retrying in 5 seconds");
+      delay(5000);
+    }
+  }
 }
 
-void webServer() {
-  // listen for incoming clients
-  WiFiClient client = server.available();
+void loop()
+{
+  // test for newæ client
+  client = server.available();
   if (client) {
+    Serial.println("New client");
+    currentLine = "";
     clientConnectTime = millis();
-    while (client.connected() && millis - clientConnectTime < CLIENT_TIMEOUT) {
-      Serial.println("Connected to client");
+    while (client.connected() && millis() - clientConnectTime < CLIENT_TIMEOUT) {
       if (client.available()) {
         char c = client.read();
         Serial.write(c);
 
-        if (currentLine.length() == 0) {
-              Serial.println("Sending response");
+        if (c == '\n') {
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            Serial.println("Sending response");
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type: text/html");
             sprintf (s, "Content-Length: %d",strlen(response_s));
@@ -94,34 +161,66 @@ void webServer() {
             client.println(response_s);
             client.println();
             response_s[0] = 0;
-        } else {
-          if (currentLine.startsWith("GET /led/red/on")) {
-            Serial.println("Red LED on");
-            digitalWrite(PIN_LED_RED, HIGH);
-          } else if (currentLine.startsWith("GET /led/red/off")) {
-            Serial.println("Red LED off");
-            digitalWrite(PIN_LED_RED, LOW);
-          }
+          } else {
+
+            if (currentLine.startsWith("GET /led/red/on")) {
+              Serial.println("Red LED on");
+              digitalWrite(PIN_LED_RED, HIGH);
+            } else if (currentLine.startsWith("GET /led/red/off")) {
+              Serial.println("Red LED off");
+              digitalWrite(PIN_LED_RED, LOW);
+    
+            } else if (currentLine.startsWith("GET /led/yellow/on")) {
+              Serial.println("Yellow LED on");
+              digitalWrite(PIN_LED_YELLOW, HIGH);
+            } else if (currentLine.startsWith("GET /led/yellow/off")) {
+              Serial.println("Yellow LED off");
+              digitalWrite(PIN_LED_YELLOW, LOW);
+    
+            } else if (currentLine.startsWith("GET /led/green/on")) {
+              Serial.println("Green LED on");
+              digitalWrite(PIN_LED_GREEN, HIGH);
+            } else if (currentLine.startsWith("GET /led/green/off")) {
+              Serial.println("Green LED off");
+              digitalWrite(PIN_LED_GREEN, LOW);
+
+            } else if (currentLine.startsWith("GET /button/a/count")) {
+              Serial.println("Return button count");
+              sprintf (response_s, "%d", button_a_count);
+              button_a_count = 0;
+            }        
+
+            currentLine = "";
+          }          
+        } else if (c != '\r') {
+          currentLine += c;          
         }
       }
     }
-
-    // close the connection:
     client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
+  if (!mqttClient.connected()) {
+    reconnect();
+  }
+  // Publish a message to MQTT broker
+  buttonCount();
+
+  // Wait for MQTT messages
+  mqttClient.loop();
 }
 
-void setup() {
-  // serial
-  Serial.begin(115200);
-  delay(10);
-
-  setupLEDS();
-  setupWifi();
-  
-
-}
-
-void loop() {
-
+void reconnect(){
+  while (!mqttClient.connected()) {
+    Serial.println("Connecting to MQTT broker...");
+    if (mqttClient.connect("ESP8266Client")) {
+      Serial.println("Connected to MQTT broker");
+    } else {
+      Serial.print("Failed to connect to MQTT broker, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" retrying in 5 seconds");
+      delay(5000);
+    }
+  }
 }
