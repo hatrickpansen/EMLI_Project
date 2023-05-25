52c8d6fdd6c085fcd2e6bea8c142b930e5bd1b05
hatrickpansen <55102319+hatrickpansen@users.noreply.github.com>
2023-05-25 10:33:45 +0200

    refacotr

--- rpi/parse_sensors.sh
+++ rpi/parse_sensors.sh
@@ -3,30 +3,36 @@
 PORT=/dev/ttyACM0 # something
 BAUDRATE=115200
 
-timeout 3s cat "$PORT" | while IFS=, read -r plant_water_alarm pump_water_alarm moisture light; do
-  if [ ! -z "$plant_water_alarm" ]; then
-    mosquitto_pub -h localhost -t pico/plant_water_alarm -m "$plant_water_alarm"
-    mosquitto_pub -h localhost -t pico/pump_water_alarm -m "$pump_water_alarm"
-    mosquitto_pub -h localhost -t pico/moisture -m "$moisture"
-    mosquitto_pub -h localhost -t pico/light -m "$light"
-    if [ "$moisture" -lt 50 ] && [ "$pump_water_alarm" -eq 1 ] && [ "$plant_water_alarm" -eq 0 ]; then
-        echo "Moisture is below 50"
-        curl -X GET http://192.168.10.222/led/red/off
-        curl -X GET http://192.168.10.222/led/green/off
-        curl -X GET http://192.168.10.222/led/yellow/on
-        # Additional actions or commands to be performed when moisture is below 50
-    elif [ "$pump_water_alarm" -eq 0 ] || [ "$plant_water_alarm" -eq 1 ]; then
-        echo "pump_water_alarm is 0 or plant_water_alarm i 1"
-        curl -X GET http://192.168.10.222/led/green/off
-        curl -X GET http://192.168.10.222/led/yellow/off
-        curl -X GET http://192.168.10.222/led/red/on
-        # Perform actions specific to this condition
-    else
-        echo "Evrything good"
-        curl -X GET http://192.168.10.222/led/red/off
-        curl -X GET http://192.168.10.222/led/yellow/off
-        curl -X GET http://192.168.10.222/led/green/on
+parse_sensors() {
+    timeout 3s cat "$PORT" | while IFS=, read -r plant_water_alarm pump_water_alarm moisture light; do
+    if [ ! -z "$plant_water_alarm" ]; then
+        mosquitto_pub -h localhost -t pico/plant_water_alarm -m "$plant_water_alarm"
+        mosquitto_pub -h localhost -t pico/pump_water_alarm -m "$pump_water_alarm"
+        mosquitto_pub -h localhost -t pico/moisture -m "$moisture"
+        mosquitto_pub -h localhost -t pico/light -m "$light"
+        if [ "$moisture" -lt 50 ] && [ "$pump_water_alarm" -eq 1 ] && [ "$plant_water_alarm" -eq 0 ]; then
+            echo "Moisture is below 50"
+            curl -X GET http://192.168.10.222/led/red/off
+            curl -X GET http://192.168.10.222/led/green/off
+            curl -X GET http://192.168.10.222/led/yellow/on
+            # Additional actions or commands to be performed when moisture is below 50
+        elif [ "$pump_water_alarm" -eq 0 ] || [ "$plant_water_alarm" -eq 1 ]; then
+            echo "pump_water_alarm is 0 or plant_water_alarm i 1"
+            curl -X GET http://192.168.10.222/led/green/off
+            curl -X GET http://192.168.10.222/led/yellow/off
+            curl -X GET http://192.168.10.222/led/red/on
+            # Perform actions specific to this condition
+        else
+            echo "Evrything good"
+            curl -X GET http://192.168.10.222/led/red/off
+            curl -X GET http://192.168.10.222/led/yellow/off
+            curl -X GET http://192.168.10.222/led/green/on
+        fi
     fi
-  fi
+    done
+}
 
-done
+while true; do
+    parse_sensors
+    sleep 1
+done;
