7e0000038b8fd2fde1118a16f91598e75a04f049
hatrickpansen <55102319+hatrickpansen@users.noreply.github.com>
2023-05-25 10:37:37 +0200

    ( . Y . )

--- rpi/parse_sensors.sh
+++ rpi/parse_sensors.sh
@@ -10,13 +10,13 @@ parse_sensors() {
         mosquitto_pub -h localhost -t pico/pump_water_alarm -m "$pump_water_alarm"
         mosquitto_pub -h localhost -t pico/moisture -m "$moisture"
         mosquitto_pub -h localhost -t pico/light -m "$light"
-        if [ "$moisture" -lt 50 ] && [ "$pump_water_alarm" -eq 1 ] && [ "$plant_water_alarm" -eq 0 ]; then
+        if [ "$moisture" -lt 50 ] && [ "$pump_water_alarm" -eq 0 ] && [ "$plant_water_alarm" -eq 1 ]; then
             echo "Moisture is below 50"
             curl -X GET http://192.168.10.222/led/red/off
             curl -X GET http://192.168.10.222/led/green/off
             curl -X GET http://192.168.10.222/led/yellow/on
             # Additional actions or commands to be performed when moisture is below 50
-        elif [ "$pump_water_alarm" -eq 0 ] || [ "$plant_water_alarm" -eq 1 ]; then
+        elif [ "$pump_water_alarm" -eq 1 ] || [ "$plant_water_alarm" -eq 0 ]; then
             echo "pump_water_alarm is 0 or plant_water_alarm i 1"
             curl -X GET http://192.168.10.222/led/green/off
             curl -X GET http://192.168.10.222/led/yellow/off
